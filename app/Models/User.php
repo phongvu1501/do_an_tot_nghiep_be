e6eb5a630e56f47106124a50cf3539be1c5002d1
HuyNguyen <huydthalamarid@gmail.com>
2025-10-19 23:59:22 +0700

    Sửa config
    :wq

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,12 +18,16 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+
     protected $fillable = [
         'name',
         'email',
         'phone',
         'password',
+        'role',
     ];
+
+
 
     /**
      * The attributes that should be hidden for serialization.
