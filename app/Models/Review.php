4e5afd5f7f9fe56e087cdf2fd36d464fcb8e6113
phituananh <anhptph53106@gmail.com>
2025-11-15 00:41:28 +0700

    thêm Review model oft deletes & relationships

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,30 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'reviews';
 
     protected $fillable = [
         'user_id',
         'reservation_id',
         'rating',
-        'comment'
+        'comment',
     ];
 
     protected $casts = [
         'rating' => 'integer',
     ];
 
-    // Người đánh giá
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    // Lần đặt bàn được đánh giá
     public function reservation(): BelongsTo
     {
         return $this->belongsTo(Reservation::class);
